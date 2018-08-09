248d7ffe93d9120293caac2fdb09da8b4f084b9f
ITCSs Developer <itcss.developer.me@gmail.com>
2018-08-09 12:30:54 +0700

    Note

---
+++ Web_VueJS_x_Razor_x_jQValidate/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Web_VueJS_x_Razor_x_jQValidate.Models;
+
+namespace Web_VueJS_x_Razor_x_jQValidate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var homeModel = new HomeModel();
+            homeModel.CustomerInfo = new List<CustomerInfo>
+            {
+                new CustomerInfo{
+                    BirthDate = DateTime.Now
+                },
+            };
+
+            return View(homeModel);
+        }
+    }
+}
---
+++ Web_VueJS_x_Razor_x_jQValidate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_VueJS_x_Razor_x_jQValidate.MvcApplication" Language="C#" %>
